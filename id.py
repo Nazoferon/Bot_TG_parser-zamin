fd5ea0b72afd6cc3d600add6cb6dae2447743569
BannyB <76437962+Nazoferon@users.noreply.github.com>
2023-03-03 16:44:23 +0200

    Update id.py

--- id.py
+++
@@ -1,8 +0,0 @@
-import telebot
-bot = telebot.TeleBot('1558588326:AAGZ-OO0CAOa-FtU70SliQH6-NfR_OaU410')
-
-@bot.message_handler(commands=['/start'])
-def fovno_message(message):
-	bot.send_message(message.chat.id, message.chat.id)
-
-bot.polling()
